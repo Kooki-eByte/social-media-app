0777e08db88ac789b9939655fded2ed142103728
Cristian Hornedo <officialcristianl@gmail.com>
2020-12-07 00:40:51 -0500

    Added validation for registering and login, Created mutations for creating a user and logging in to one. Made it a more MVC structure.

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,17 @@
+const postsResolvers = require("./posts");
+const usersResolvers = require("./users");
+
+/*
+  @ DESCRIPTION:  This will be the resolve for either a 
+                  query, mutation, or subscription being called from 
+                  typeDefs. This pretty much is graphQLs controller.
+*/
+
+module.exports = {
+  Query: {
+    ...postsResolvers.Query,
+  },
+  Mutation: {
+    ...usersResolvers.Mutation,
+  },
+};
