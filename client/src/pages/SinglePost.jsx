32d0ff92c63e2a18eba14341b05f559593ca04bf
Cristian Hornedo <officialcristianl@gmail.com>
2021-01-05 15:38:06 -0500

    Added in the comment functionality

--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -1,5 +1,5 @@
-import { useQuery } from "@apollo/react-hooks";
-import { Badge, Grid } from "@material-ui/core";
+import { useMutation, useQuery } from "@apollo/react-hooks";
+import { Badge, Button, Grid, Input } from "@material-ui/core";
 import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -13,7 +13,7 @@ import CommentIcon from "@material-ui/icons/Comment";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import gql from "graphql-tag";
 import moment from "moment";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import DeleteButton from "../components/DeleteButton";
 import LikeButton from "../components/LikeButton";
@@ -37,6 +37,21 @@ const FETCH_SINGLE_POST = gql`
         createdAt
         body
       }
+    }
+  }
+`;
+
+const SUBMIT_COMMENT_MUTATION = gql`
+  mutation($postId: ID!, $body: String!) {
+    createComment(postId: $postId, body: $body) {
+      id
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+      commentCount
     }
   }
 `;
@@ -71,6 +86,8 @@ export default function SinglePost(props) {
   const classes = useStyles();
   const postId = props.match.params.postId;
 
+  const [comment, setComment] = useState("");
+
   const deletePostCallback = () => {
     props.history.push("/");
   };
@@ -78,6 +95,16 @@ export default function SinglePost(props) {
   const { data } = useQuery(FETCH_SINGLE_POST, {
     variables: {
       postId,
+    },
+  });
+
+  const [submitComment] = useMutation(SUBMIT_COMMENT_MUTATION, {
+    update() {
+      setComment("");
+    },
+    variables: {
+      postId,
+      body: comment,
     },
   });
 
@@ -97,21 +124,16 @@ export default function SinglePost(props) {
       commentCount,
     } = data.getPost;
 
+    console.log(comments);
+
     postMarkUp = (
       <Grid container style={{ marginTop: 20 }}>
-        <Grid
-          item
-          xs={3}
-          sm={3}
-          md={3}
-          lg={3}
-          style={{ alignSelf: "center", paddingLeft: "8%" }}
-        >
+        <Grid item xs={3} sm={3} md={3} lg={3} style={{ paddingLeft: "8%" }}>
           <Avatar aria-label="recipe" className={classes.avatar}>
             {username[0]}
           </Avatar>
         </Grid>
-        <Grid item xs={12} sm={12} md={9} lg={9}>
+        <Grid item xs={12} sm={12} md={8} lg={8}>
           <Card className={classes.root}>
             <CardHeader
               title={username}
@@ -142,6 +164,53 @@ export default function SinglePost(props) {
               )}
             </CardActions>
           </Card>
+          {user && (
+            <Card className={classes.root} style={{ marginTop: 10 }}>
+              <CardContent>
+                <p>Post a comment</p>
+                <form className={classes.root} noValidate>
+                  <Input
+                    type="text"
+                    placeholder="message..."
+                    name="comment"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    className="MuiInput-root"
+                    style={{ width: "75%" }}
+                  />
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    disabled={comment.trim() === ""}
+                    onClick={submitComment}
+                  >
+                    Send🐞
+                  </Button>
+                </form>
+              </CardContent>
+            </Card>
+          )}
+          {comments &&
+            comments.map((comment) => (
+              <Card className={classes.root} style={{ marginTop: 10 }}>
+                <CardContent>
+                  <Typography variant="h6" component="h2">
+                    {comment.username}
+                  </Typography>
+                  <Typography className={classes.pos} color="textSecondary">
+                    {moment(comment.createdAt).fromNow()}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {comment.body}
+                  </Typography>
+                </CardContent>
+                {user && user.username === comment.username && (
+                  <CardActions>
+                    <DeleteButton postId={postId} commentId={comment.id} />
+                  </CardActions>
+                )}
+              </Card>
+            ))}
         </Grid>
       </Grid>
     );
